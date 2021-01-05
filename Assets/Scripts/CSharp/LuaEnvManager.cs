831572084bb00bb033e383dc72a846b24d6f74d0
liangfeifei <liangfeifei@topjoy.com>
2021-01-05 11:17:15 +0800

    init

---
+++ Assets/Scripts/CSharp/LuaEnvManager.cs
@@ -0,0 +1,25 @@
+using XLua;
+
+namespace DefaultNamespace
+{
+    public class LuaEnvManager
+    {
+        private static LuaEnvManager instance;
+        private static LuaEnv luaEnv = new LuaEnv();
+
+        static LuaEnvManager()
+        {
+            instance = new LuaEnvManager();
+        }
+        
+        public static LuaEnvManager Instance
+        {
+            get => instance;
+        }
+
+        public LuaEnv GetEnv()
+        {
+            return luaEnv;
+        }
+    }
+}
